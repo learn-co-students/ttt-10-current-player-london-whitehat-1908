5ce86e13fecb2e620ad5ef999e8d81482e00c1bd
Oliver Parkinson <olliepar01@gmail.com>
2019-08-08 22:20:06 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,15 +8,11 @@
   return turns
 end
 
-def curren
-  t_player(board)
+def current_player(board)
   turns = turn_count(board)
-  turns = turns - 1
-  if turns==0 then 
+  if turns%2==0 then
     return "X"
-  elsif turns%1==0 then
-    return "X"
-  elsif turns%1==0 then 
+  else
     return "O"
   end
   
